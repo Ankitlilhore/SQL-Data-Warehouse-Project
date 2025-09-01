17b3df33913fe2b30bb616b0b88ca8dba0dbea57
Ankitlilhore <128494246+Ankitlilhore@users.noreply.github.com>
2025-09-01 22:41:35 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,20 @@
+
+---------------------------------------------------
+-- Database 'DataWarehouse' created here
+-- Created three schemas bronz, silver and gold 
+---------------------------------------------------
+
+CREATE DATABASE DataWarehouse
+
+
+
+USE DataWarehouse
+
+CREATE SCHEMA bronze
+GO
+
+CREATE SCHEMA silver
+GO
+
+CREATE SCHEMA gold
+GO
